acda462ae60d9b3f0a85a2f4994a35f615a03210
Alva <asjun2lin@outlook.com>
2022-08-22 23:59:22 +0800

    创建项目

---
+++ server/src/Alva.KuCloud.Api/Controllers/HealthController.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Alva.KuCloud.Api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class HealthController : ControllerBase
+{
+    private readonly ILogger<HealthController> _logger;
+
+    public HealthController(ILogger<HealthController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet("[action]")]
+    public IActionResult Heartbeat()
+    {
+        return Ok();
+    }
+}
