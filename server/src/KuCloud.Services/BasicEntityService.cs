cb083cd65d41e6b848d6eb7b73ec1963b0c4852d
Alva <asjun2lin@outlook.com>
2022-09-01 23:14:53 +0800

    BasicEntityService 的方法默认不保存，在释放时保存

--- server/src/KuCloud.Services/BasicEntityService.cs
+++ server/src/KuCloud.Services/BasicEntityService.cs
@@ -8,7 +8,7 @@ using System.Linq.Expressions;
 
 namespace KuCloud.Services;
 
-public abstract class BasicEntityService<TEntity> : IBasicEntityService<TEntity> where TEntity : class, IBasicEntity
+public abstract class BasicEntityService<TEntity> : IBasicEntityService<TEntity>, IDisposable where TEntity : class, IBasicEntity
 {
     protected BasicEntityService(DbContext dbContext)
     {
@@ -29,54 +29,59 @@ public abstract class BasicEntityService<TEntity> : IBasicEntityService<TEntity>
     /// <param name="cancellationToken"></param>
     /// <param name="saveNow">默认值为 true, 如果传一个空值，那么会根据 this.SaveNow 来判断是否立刻保存</param>
     /// <returns>受影响的实体数量</returns>
-    public virtual async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default, bool? saveNow = true)
+    public virtual async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default, bool? saveNow = null)
     {
         if (saveNow ?? SaveNow)
         {
-            return await DbContext.SaveChangesAsync(cancellationToken);
+            return await DbContext.SaveChangesAsync(cancellationToken).ConfigureAwait(false);
         }
         return 0;
+    }
+
+    public void Dispose()
+    {
+        SaveChangesAsync(saveNow: true).Wait();
     }
 
     public virtual Task<List<TEntity>> GetAllAsync(CancellationToken cancellationToken = default) =>
         DbSet.ToListAsync(cancellationToken);
 
-    public Task<List<TEntity>> WhereAsync(Expression<Func<TEntity, bool>> predicate, CancellationToken cancellationToken) =>
+    public Task<List<TEntity>> WhereAsync(Expression<Func<TEntity, bool>> predicate, CancellationToken cancellationToken = default) =>
         DbSet.Where(predicate).ToListAsync(cancellationToken: cancellationToken);
 
-    public Task<TEntity?> FindAsync(Expression<Func<TEntity, bool>> predicate, CancellationToken cancellationToken) =>
+    public Task<TEntity?> FindAsync(Expression<Func<TEntity, bool>> predicate, CancellationToken cancellationToken = default) =>
         DbSet.FirstOrDefaultAsync(predicate, cancellationToken);
 
-    public async Task<TEntity> GetAsync(Expression<Func<TEntity, bool>> predicate, CancellationToken cancellationToken) =>
+    public async Task<TEntity> GetAsync(Expression<Func<TEntity, bool>> predicate, CancellationToken cancellationToken = default) =>
         await FindAsync(predicate, cancellationToken) ?? throw new EntityNotFoundException(typeof(TEntity), predicate);
 
-    public virtual async Task<TEntity> AddAsync(TEntity entity, CancellationToken cancellationToken = default, bool? saveNow = null)
+    public virtual async Task<TEntity> AddAsync(TEntity entity, CancellationToken cancellationToken = default, bool? saveNow = false)
     {
         var entry = await DbSet.AddAsync(entity, cancellationToken);
         await SaveChangesAsync(cancellationToken, saveNow);
         return entry.Entity;
     }
 
-    public virtual async Task<TEntity> UpdateAsync(TEntity entity, CancellationToken cancellationToken = default, bool? saveNow = null)
+    public virtual async Task<TEntity> UpdateAsync(TEntity entity, CancellationToken cancellationToken = default, bool? saveNow = false)
     {
         var entry = DbSet.Update(entity);
         await SaveChangesAsync(cancellationToken, saveNow);
         return entry.Entity;
     }
 
-    public virtual async Task UpdateRangeAsync(IEnumerable<TEntity> entities, CancellationToken cancellationToken = default, bool? saveNow = null)
+    public virtual async Task UpdateRangeAsync(IEnumerable<TEntity> entities, CancellationToken cancellationToken = default, bool? saveNow = false)
     {
         DbSet.UpdateRange(entities);
         await SaveChangesAsync(cancellationToken, saveNow);
     }
 
-    public virtual async Task DeleteAsync(TEntity entity, CancellationToken cancellationToken = default, bool? saveNow = null)
+    public virtual async Task DeleteAsync(TEntity entity, CancellationToken cancellationToken = default, bool? saveNow = false)
     {
         DbSet.Remove(entity);
         await SaveChangesAsync(cancellationToken, saveNow);
     }
 
-    public virtual async Task DeleteAsync(IEnumerable<TEntity> entities, CancellationToken cancellationToken = default, bool? saveNow = null)
+    public virtual async Task DeleteAsync(IEnumerable<TEntity> entities, CancellationToken cancellationToken = default, bool? saveNow = false)
     {
         DbSet.RemoveRange(entities);
         await SaveChangesAsync(cancellationToken, saveNow);
@@ -98,7 +103,7 @@ public abstract class BasicEntityService<TEntity, TKey> : BasicEntityService<TEn
     public virtual async Task<TEntity> GetAsync(TKey id, CancellationToken cancellationToken = default) =>
         await FindAsync(id, cancellationToken) ?? throw new EntityNotFoundException(typeof(TEntity), id);
 
-    public virtual async Task DeleteAsync(TKey id, CancellationToken cancellationToken = default, bool? saveNow = null)
+    public virtual async Task DeleteAsync(TKey id, CancellationToken cancellationToken = default, bool? saveNow = false)
     {
         var entity = await FindAsync(id, cancellationToken);
         if (entity is not null)
@@ -107,7 +112,7 @@ public abstract class BasicEntityService<TEntity, TKey> : BasicEntityService<TEn
         }
     }
 
-    public virtual async Task DeleteAsync(IEnumerable<TKey> ids, CancellationToken cancellationToken = default, bool? saveNow = null)
+    public virtual async Task DeleteAsync(IEnumerable<TKey> ids, CancellationToken cancellationToken = default, bool? saveNow = false)
     {
         var entities = await DbSet.Where(entity => ids.Contains(entity.Id)).ToListAsync(cancellationToken);
         await DeleteAsync(entities, cancellationToken);
